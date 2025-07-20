05a23974de709ba23be681673e8360beef127799
Gabriel Ribeiro <gabriel.rf.business@gmail.com>
2025-07-19 21:42:00 -0300

    Delete 17 - Manufacturers with Average Price Above 145 and At Least 2 Products Query.sql

--- 17 - Manufacturers with Average Price Above 145 and At Least 2 Products Query.sql
+++
@@ -1,8 +0,0 @@
--- Exercise 017:
--- Select the name of each manufacturer which have an average price above $145 and contain at least 2 different products. 
-
-Select m.Name, Avg(p.price) as p_price, COUNT(p.Manufacturer) as m_count
-FROM Manufacturers m, Products p
-WHERE p.Manufacturer = m.code
-GROUP BY m.Name , p.Manufacturer
-HAVING Avg(p.price) >= 150 and COUNT(p.Manufacturer) >= 2;
