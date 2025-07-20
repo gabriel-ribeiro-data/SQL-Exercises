684ef00a4b8d5a98832d76b6109cfbf953077a77
Gabriel Ribeiro <gabriel.rf.business@gmail.com>
2025-07-19 21:41:22 -0300

    Delete 11 - Product Information Query.sql

--- 11 - Product Information Query.sql
+++
@@ -1,6 +0,0 @@
--- Exercise 011:
--- Select the product name, price, and manufacturer name of all the products.
-
-SELECT Products.Name, Price, Manufacturers.Name
-FROM Products, Manufacturers
-WHERE Products.Manufacturer = Manufacturers.Code;
