33d0c30ec6714e70a6bfbf5a312198df5d7c6245
Gabriel Ribeiro <gabriel.rf.business@gmail.com>
2025-07-19 21:41:28 -0300

    Delete 12 - Average Price by Manufacturer Query.sql

--- 12 - Average Price by Manufacturer Query.sql
+++
@@ -1,6 +0,0 @@
--- Exercise 012:
--- Select the average price of each manufacturer's products, showing only the manufacturer's code.
-
-SELECT AVG(Price), Manufacturer
-FROM Products
-GROUP BY Manufacturer;
